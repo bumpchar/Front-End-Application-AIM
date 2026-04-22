f5d094622605ba9c337755c5df7db3903b83c186
bumpchar <bumpchar@gmail.com>
2026-04-22 14:57:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="NBA News App",
+    page_icon="🏀",
+    layout="wide"
+)
 st.title("NBA News App")
-st.write("AIM App")
+st.write("Use the sidebar to navigate through the app.")

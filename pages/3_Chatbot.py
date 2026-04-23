bc483c18200ddabdc0f7514e42a5cd352c33e924
EoinRedmond <eoin.redmond@marquette.edu>
2026-04-23 15:54:44 -0500

    Update 3_Chatbot.py

--- pages/3_Chatbot.py
+++ pages/3_Chatbot.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.title("Welcome to my app!")
-st.write("This is how to build simple front end python applications.")
+st.title("Welcome to our Chatbot!")
+st.write("This our ChatBot.")
